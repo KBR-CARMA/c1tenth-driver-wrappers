22ee0b49290fee83cbdbacc9baaf1750e57b338a
nickm1824 <114448418+nickm1824@users.noreply.github.com>
2023-02-10 17:11:17 -0500

    Update sllidar_ros2_driver_wrapper_launch.py

--- sllidar_ros2_driver_wrapper/launch/sllidar_ros2_driver_wrapper_launch.py
+++ sllidar_ros2_driver_wrapper/launch/sllidar_ros2_driver_wrapper_launch.py
@@ -15,27 +15,27 @@ sllidar_param_file = os.path.join(
 sllidar_wrapper_param_file = os.path.join(
   c1tenth_launch_pkg_prefix, 'param/sllidar.param.yaml')
 
+def generate_launch_description():
+  sllidar_node = Node(
+          package='sllidar_ros2',
+          executable='sllidar_node',
+          name='sllidar_node',
+          namespace='lidar',
+          parameters=[sllidar_driver_param_file],
+          output='screen',
+          remappings=[
+              ('scan', 'points_raw')
+          ]
+      )
 
-sllidar_node = Node(
-        package='sllidar_ros2',
-        executable='sllidar_node',
-        name='sllidar_node',
-        namespace='lidar',
-        parameters=[sllidar_driver_param_file],
-        output='screen',
-        remappings=[
-            ('scan', 'points_raw')
-        ]
-    )
-
-sllidar_ros2_driver_wrapper_node = Node(
-        package='sllidar_ros2_driver_wrapper',
-        executable='sllidar_ros2_driver_wrapper_node_exec',
-        name='sllidar_ros2_driver_wrapper_node',
-        namespace='lidar',
-        parameters=[sllidar_wrapper_param_file],
-        output='screen'
-    )
+  sllidar_ros2_driver_wrapper_node = Node(
+          package='sllidar_ros2_driver_wrapper',
+          executable='sllidar_ros2_driver_wrapper_node_exec',
+          name='sllidar_ros2_driver_wrapper_node',
+          namespace='lidar',
+          parameters=[sllidar_wrapper_param_file],
+          output='screen'
+      )
 
 return launch.LaunchDescription([
         sllidar_node,
