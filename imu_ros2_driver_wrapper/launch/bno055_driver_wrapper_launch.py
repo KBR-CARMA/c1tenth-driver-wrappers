fc9413b8b3b864ee594404525a7e49dc4f0994d5
jalesseVolpe <john.alesse.ctr@dot.gov>
2023-02-12 17:52:46 -0500

    Fixups for params files. Builds Clean

--- imu_ros2_driver_wrapper/launch/bno055_driver_wrapper_launch.py
+++ imu_ros2_driver_wrapper/launch/bno055_driver_wrapper_launch.py
@@ -5,27 +5,30 @@ from launch.conditions import IfCondition
 from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import ComposableNodeContainer
 from launch_ros.descriptions import ComposableNode
-from carma_ros2_utils.launch.get_current_namespace import GetCurrentNamespace
 from ament_index_python import get_package_share_directory
 import os
+
+
+bno055_pkg_prefix = get_package_share_directory('bno055'),
+
 # TODO: remapings, params, names
 bno055_driver_param_file = os.path.join(
-  c1tenth_launch_pkg_prefix, 'params/bno055.params.yaml')
+  get_package_share_directory('bno055'), 'config', 'bno055.params.yaml')
 
-sllidar_wrapper_param_file = os.path.join(
-  c1tenth_launch_pkg_prefix, 'param/bno055_driver_wrapper.param.yaml')
+bno055_wrapper_param_file = os.path.join(
+  get_package_share_directory('bno055_driver_wrapper'), 'config', 'bno055_driver_wrapper.params.yaml')
 
 def generate_launch_description():
   bno055_driver_node = Node(
           package='bno055',
           executable='bno055',
-          name='sllidar_node',
+          name='bno055_node',
           namespace='bno055',
           parameters=[bno055_driver_param_file],
-          output='screen',
-          remappings=[
-              ('scan', 'points_raw')
-          ]
+          output='screen'
+#          remappings=[
+#              ('scan', 'points_raw')
+#          ]
       )
 
   bno055_driver_wrapper_node = Node(
